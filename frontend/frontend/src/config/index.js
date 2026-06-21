1bf78a139878df5896334aa43806205bd455094a
Sudhakar Kumar <kumarsudhakar0815@gmail.com>
2026-06-22 01:08:57 +0530

    restructure my proj

--- frontend/frontend/src/config/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-
-const client = axios.create({
-    baseURL: "http://localhost:9090"
-})
-
-export default client;
--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export const BASE_URL = "http://localhost:9090"
-const client = axios.create({
-    baseURL: BASE_URL
-})
-
-export default client;
